c273469ae368db7f3b71bd5eb4903b86d670c08b
Carles Capellas <capellas.carles@gmail.com>
2020-04-11 10:18:24 +0200

    chore: rename cell to box

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -9,8 +9,8 @@ export const Grid: React.FC<GridProps> = (props) => {
         <div className={`sudoku-grid size-${props.size}`}>
             {[...Array(props.size)].map((_x, rowIndex) => (
                 <div className="sudoku-row">
-                    {[...Array(props.size)].map((_y, cellIndex) => (
-                        <div className="sudoku-cell">
+                    {[...Array(props.size)].map((_y, boxIndex) => (
+                        <div className="sudoku-box">
                             {[...Array(props.size)].map((_z, candidateIndex) => (
                                 <div className="sudoku-candidate">{candidateIndex + 1}</div>
                             ))}
