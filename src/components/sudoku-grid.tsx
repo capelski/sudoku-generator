65a4b80f12002f5b863066ebc5ac67619e9b0098
Carles Capellas <capellas.carles@gmail.com>
2020-04-11 10:52:48 +0200

    feat: highlight peer candidates on candidate click

--- src/components/sudoku-grid.tsx
+++ src/components/sudoku-grid.tsx
@@ -1,22 +1,61 @@
-import React from 'react';
-import { Sudoku } from '../types/sudoku';
+import React, { useState } from 'react';
+import { Sudoku, Box } from '../types/sudoku';
 
 interface GridProps {
     sudoku: Sudoku;
 }
 
+interface BoxNumber {
+    box: Box;
+    number: number;
+}
+
 export const SudokuGrid: React.FC<GridProps> = (props) => {
+    const [selectedBoxNumber, setSelectedBoxNumber] = useState<BoxNumber | undefined>(undefined);
+
     return (
         <div className={`sudoku-grid size-${props.sudoku.size}`}>
             {props.sudoku.boxes.map((row) => (
                 <div className="sudoku-row">
-                    {row.map((box) => (
-                        <div className="sudoku-box">
-                            {box.candidates.map((candidate) => (
-                                <div className="sudoku-candidate">{candidate.number}</div>
-                            ))}
-                        </div>
-                    ))}
+                    {row.map((box) => {
+                        const isPeerBox =
+                            selectedBoxNumber !== undefined &&
+                            (selectedBoxNumber.box.column === box.column ||
+                                selectedBoxNumber.box.region === box.region ||
+                                selectedBoxNumber.box.row === box.row);
+                        return (
+                            <div className="sudoku-box">
+                                {box.candidates.map((candidate) => {
+                                    const isSelectedCandidate =
+                                        selectedBoxNumber !== undefined &&
+                                        selectedBoxNumber.box === box &&
+                                        selectedBoxNumber.number === candidate.number;
+
+                                    const mustHighlightCandidate =
+                                        isPeerBox && selectedBoxNumber!.number === candidate.number;
+
+                                    const candidateClickHandler = () => {
+                                        setSelectedBoxNumber({ box, number: candidate.number });
+                                    };
+
+                                    return (
+                                        <div
+                                            className={`sudoku-candidate${
+                                                isSelectedCandidate
+                                                    ? ' selected'
+                                                    : mustHighlightCandidate
+                                                    ? ' highlight'
+                                                    : ''
+                                            }`}
+                                            onClick={candidateClickHandler}
+                                        >
+                                            {candidate.number}
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        );
+                    })}
                 </div>
             ))}
         </div>
